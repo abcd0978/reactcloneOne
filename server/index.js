257d31061fed071f04b899f91eafa8d7a0f41865
kimmingyu <kimmin36944@gmail.com>
2022-02-05 19:03:10 +0900

    add: comment router added

--- server/index.js
+++ server/index.js
@@ -11,14 +11,15 @@ const path = require('path')
 const userRouter = require('./routes/user');
 const videoRouter = require('./routes/video');
 const subscribeRouter = require('./routes/subscribe');
+const commentRouter = require('./routes/comment')
 
 dotenv.config();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 app.use(cookieParser())
-console.log(path.join(__dirname,'../uploads'))
-app.use('/uploads',express.static(path.join(__dirname,'../uploads')));
+
+app.use('/uploads',express.static(path.join(__dirname,'../uploads')));//정적주소
 
 const port = process.env.PORT
 mongoose.connect(process.env.MONGO_URL)
@@ -29,9 +30,10 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(err)
 })
 
-app.use('/api/users',userRouter)
-app.use('/api/video',videoRouter)
+app.use('/api/users',userRouter);
+app.use('/api/video',videoRouter);
 app.use('/api/subscribe',subscribeRouter);
+app.use('/api/comment',commentRouter);
 
 
 app.listen(port,()=>console.log(`Example app listening on port ${port}`));
